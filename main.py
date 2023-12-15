fdb64ec2a2bc1dd8659f8c0fd100519c268c4bbe
blackundo <blackundovn@gmail.com>
2023-12-15 11:02:56 +0700

    update if wait

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from CBAutoHelper import CheckNode, LDPlayer
 import time
 import threading
 ld_path = r'E:\LDPlayer\LDPlayer3.0'
-img_path = r'.\img\done.png'
+img_path = r'.\img\screenshot_0.png'
 game_package = 'com.moonactive.coinmaster'
 
 
@@ -24,10 +24,12 @@ def run_vip(device):
         # init clear data and start app
         ld.DeleteCache(game_package)
         ld.OpenApp(game_package)
-        while True:
+        time.sleep(3)
+        start_time = time.time()
+        while time.time() - start_time < 30:
             img = ld.FindImg(img_path)
             # print(img)
-            if img[0] == 177:
+            if img[0] == 450:
                 time.sleep(3)
                 break
         ld.StopApp(game_package)
